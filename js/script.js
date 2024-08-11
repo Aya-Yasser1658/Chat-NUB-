81f613934c525ab588b58db3df00f6d4766aaaf7
Aya-Yasser1658 <109250104+Aya-Yasser1658@users.noreply.github.com>
2024-08-11 23:14:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const formOpenBtn = document.querySelector("#form-open1"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+  /*Responsive code for menu-bar*/
+let menuBar=document.querySelector("#menu-bar")
+let amenu=document.querySelector(".nav-link")
+
+function showmenu(){
+  menuBar.classList.toggle("fa-times")
+  amenu.classList.toggle("active")
+}
+
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+const observer =new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  });
+
+});
+
+const NewsElements = document.querySelectorAll('.News');
+NewsElements.forEach((el)=> observer.observe(all));
+
